fd9b69ae8a8fa9c102cca0eb763b70992d30eea0
Inna-Smertyuk <86621124+Inna-Smertyuk@users.noreply.github.com>
2022-07-31 03:13:14 +0300

    hw-03
    .

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,47 @@
-// const fs = require('fs/promises')
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
 
-const listContacts = async () => {}
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
 
-const getContactById = async (contactId) => {}
+const addContactSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string()
+    .pattern(/^((8|\+7)[- ]?)?(\(?\d{3}\)?[- ]?)?[\d\- ]{7,10}$/)
+    .required(),
+  favorite: Joi.boolean(),
+});
 
-const removeContact = async (contactId) => {}
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+});
 
-const addContact = async (body) => {}
+const schemas = {
+  add: addContactSchema,
+  updateFavorite: updateFavoriteSchema,
+};
 
-const updateContact = async (contactId, body) => {}
+const Contact = model("contact", contactSchema);
 
 module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-}
+  Contact,
+  schemas,
+};
